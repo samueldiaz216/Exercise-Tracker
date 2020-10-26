c6d610be8f07bf49aceac6983e2e6cd6d9d13b5f
samueldiaz216 <samueldiaz216@gmail.com>
2020-10-26 15:18:45 -0400

    Updates

---
+++ precache-manifest.96c43113dbbe6df1787b3dc845e53e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c0b6e9d503ba419171b00305e6b76d",
+    "url": "/Exercise-Tracker/index.html"
+  },
+  {
+    "revision": "29a7385e89f16769c7be",
+    "url": "/Exercise-Tracker/static/css/2.293e860e.chunk.css"
+  },
+  {
+    "revision": "29a7385e89f16769c7be",
+    "url": "/Exercise-Tracker/static/js/2.dec51929.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/Exercise-Tracker/static/js/2.dec51929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728472437bb21f64d67c",
+    "url": "/Exercise-Tracker/static/js/main.ce732b10.chunk.js"
+  },
+  {
+    "revision": "a93b09568090b9200f3d",
+    "url": "/Exercise-Tracker/static/js/runtime-main.52f1c284.js"
+  }
+]);
